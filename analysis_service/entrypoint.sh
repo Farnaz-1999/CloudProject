8d112bdd589f9e1e3489a47b5dadbb31b1258ae8
yousef salmalian <flaslnm@gmail.com>
2022-04-14 14:01:11 +0430

    fix name

--- analysis_service/entrypoint.sh
+++ analysis_service/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python3 manage.py inspectdb --database=flight_db > analysis/models.py
+python3 manage.py inspectdb --database=flight_db > analysis/replica_models.py
 python3 manage.py runserver 0.0.0.0:8000
